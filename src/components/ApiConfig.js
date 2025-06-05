dd88e1b7524ae2d5bb871e8ef89d40ad52f10607
kartikpande123 <kartikpande67@gmail.com>
2025-06-05 22:24:24 +0530

    jdc

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3200'
-    : 'https://www.naphex.com';
+    : 'https://naphex.com';
 
 export default API_BASE_URL;
