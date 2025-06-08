941360202810c75aca5b875fdc41969f2131f6ed
kartikpande123 <kartikpande67@gmail.com>
2025-06-08 12:52:38 +0530

    adding prefix

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3200'
-    : 'https://naphex.com';
+    ? 'http://localhost:3200/api'
+    : 'https://naphex.com/api';
 
 export default API_BASE_URL;
